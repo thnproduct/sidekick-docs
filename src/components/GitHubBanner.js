1e40a488bbfa8be92c3ea095dcbd43b91975b9c2
Burak Kantarcı <burak@thundra.io>
2022-08-23 15:06:35 +0300

    added gh marketplace link

---
+++ src/components/GitHubBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/custom.css';
+
+export const GitHubBanner = () => (
+  <div className="github-banner">
+    <div>
+      <img src="https://assets.website-files.com/6294d502b5093e77fbb91f34/62f10f090b52a64dd5f10858_Octocat-p-800.png" alt="GitHub Octopus" width="100"></img>
+    </div>
+    <div>
+      <h2>Discover Foresight on GitHub Marketplace</h2>
+      <a href="https://github.com/marketplace/thundra-foresight" target="_blank" className="cta">Try for free</a>
+    </div>
+  </div>
+);
