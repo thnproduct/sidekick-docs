4cb64d68148320b8cb46872b9cf0469f42ff0797
Burak Kantarcı <burak@thundra.io>
2022-09-08 15:27:13 +0300

    svg fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,6 @@ export const Card = ({children, title, target, isNewWindow}) => (
       <div className="plugin-card-title">{title}</div>
       <div className="text-gray-500">{children}</div>
     </div>
-    <svg className="w-6 h-6 float-right" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+    <svg className="w-6 h-6 float-right" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
   </a>
 );
